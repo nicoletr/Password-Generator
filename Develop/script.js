8545115efafd1b9d3fe6b2869e5b1f7d455559d8
Nicoletr <nicole.trpevski@gmail.com>
2021-09-04 12:29:39 +0800

    Added beginnings of function using prompt and confirm methods

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,36 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  var passwordLength = prompt("How long would you like your password to be? Choose a number between 8 and 128")
+  if (passwordLength < 8) {
+   passwordLength = prompt("Please choose a number between 8 and 128")
+  }else if (passwordLength > 128) {
+    passwordLength = prompt("Please choose a number between 8 and 128")
+  }else {
+    number = confirm();
+  }
+
+  var number = confirm("Would you like to include a number?")
+   if (confirm) {
+    // insert number into password
+  }else {
+    // DO NOT insert number into password
+  }
+
+  var specialCharacter = confirm("Would you like to include a special character?")
+  if (confirm) {
+   // insert special character into password
+  }else {
+   // DO NOT insert special character into password
+  }
+
+  var upperCase = confirm("Would you like to include upper case letters?")
+  if (confirm) {
+   // insert special character into password
+  }else {
+   // DO NOT insert special character into password
+  }
+
   passwordText.value = password;
 
 }
