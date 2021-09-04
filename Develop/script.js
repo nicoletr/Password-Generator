d7938a2766be56b19b8635789f99ec6936c6c7a0
Nicoletr <nicole.trpevski@gmail.com>
2021-09-04 14:08:52 +0800

    Updated some of the console.log methods

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   // Determines users desired length of password
-  var passwordLength = prompt("How long would you like your password to be? Choose a number between 8 and 128")
+  var passwordLength = window.prompt("How long would you like your password to be? Choose a number between 8 and 128")
   if (passwordLength < 8) {
    passwordLength = prompt("Please choose a number between 8 and 128")
   }else if (passwordLength > 128) {
@@ -17,7 +17,7 @@ function writePassword() {
   }
 
   //Determines if user would like to include a number in the password
-  var number = confirm("Would you like to include a number?")
+  var number = window.confirm("Would you like to include a number?")
    if (confirm) {
     console.log = (Math.random() *(8 - 128))
   }else {
@@ -27,25 +27,25 @@ function writePassword() {
   //Determines if user would like to incluse a special character in the password
   var specialCharacter = confirm("Would you like to include a special character?")
   if (confirm) {
-   // insert special character into password
+   console.log = ("")
   }else {
-   // DO NOT insert special character into password
+   console.log = ("")
   }
 
   // Determines if user would like upper case letters in the password
   var upperCase = confirm("Would you like to include upper case letters?")
   if (confirm) {
-   // insert special character into password
+   console.log = ("")
   }else {
-   // DO NOT insert special character into password
+    console.log = ("")
   }
 
   // Determines if user would like lower case letters in the password
   var lowerCase = confirm("Would you like to include lower case letters?")
   if (confirm) {
-   // insert special character into password
+    console.log = ("")
   }else {
-   // DO NOT insert special character into password
+    console.log = ("")
   }
 
   passwordText.value = password;
