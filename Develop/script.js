55c12afdb4d7733c10593190addc62b5322098a5
Nicoletr <nicole.trpevski@gmail.com>
2021-09-09 12:45:17 +0800

    Updated console.log for variables inside writePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -8,35 +8,35 @@ function writePassword() {
 
   // Determines users desired length of password
   var passwordLength = window.prompt("How long would you like your password to be? Choose a number between 8 and 128")
-  var length = passwordLength.value
+  var length = +passwordLength.value
   if (passwordLength < 8 && passwordLength > 128) {
    passwordLength = prompt("Please choose a number between 8 and 128")
   }else {
-    console.log = length;
+    console.log (length);
   }
 
   //Determines if user would like to include a number in the password
   var includeNumber = window.confirm("Would you like to include a number?")
    if (includeNumber) {
-    console.log = getRandomNumber();
+    console.log (includeNumber);
   }
 
   //Determines if user would like to incluse a special character in the password
   var includeSpecialCharacter = confirm("Would you like to include a special character?")
   if (includeSpecialCharacter) {
-   console.log = getRandomSymbol();
+   console.log (includeSpecialCharacter);
   }
 
   // Determines if user would like upper case letters in the password
   var includeUpperCase = confirm("Would you like to include upper case letters?")
   if (includeUpperCase) {
-    console.log = getRandomUpper();
+    console.log (includeUpperCase);
   }
 
   // Determines if user would like lower case letters in the password
   var includeLowerCase = confirm("Would you like to include lower case letters?")
   if (includeLowerCase) {
-    console.log = getRandomLower();
+    console.log (includeLowerCase);
   }
 
   passwordText.value = password;
