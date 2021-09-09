55c87b0d591829cd7b38464c8f58b8461b6d7da4
Nicoletr <nicole.trpevski@gmail.com>
2021-09-09 17:47:28 +0800

    Updated some indenting in the JS code

--- Develop/script.js
+++ Develop/script.js
@@ -3,21 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 var passwordLength = window.prompt("How long would you like your password to be? Choose a number between 8 and 128")
 
-  // Determines users desired length of password
-  if (passwordLength < 8 && passwordLength > 128) {
-   passwordLength = prompt("Number must be between 8 and 128")
-  }
+// Determines users desired length of password
+if (passwordLength < 8 && passwordLength > 128) {
+ passwordLength = prompt("Number must be between 8 and 128")
+}
 
-  //Below determines what the user would like included in the password
-  if (passwordLength > 8 && passwordLength < 128) {
-  var includeNumber = window.confirm("Would you like to include a number?")
-  var includeSpecialCharacter = confirm("Would you like to include a special character?")
-  var includeUpperCase = confirm("Would you like to include upper case letters?")
-  var includeLowerCase = confirm("Would you like to include lower case letters?")
-  }
+//Below determines what the user would like included in the password
+if (passwordLength >= 8 && passwordLength <= 128) {
+var includeNumber = window.confirm("Would you like to include a number?")
+var includeSpecialCharacter = confirm("Would you like to include a special character?")
+var includeUpperCase = confirm("Would you like to include upper case letters?")
+var includeLowerCase = confirm("Would you like to include lower case letters?")
+}
 
-  if (includeNumber != true && includeSpecialCharacter != true && includeUpperCase != true && includeLowerCase != true){
-    confirm("You must select at least one character type")
+if (includeNumber != true && includeSpecialCharacter != true && includeUpperCase != true && includeLowerCase != true){
+  confirm("You must select at least one character type")
 }
 
 
@@ -38,7 +38,7 @@ function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 function getRandomSymbol() {
   const symbols = "!@#$%^&*(){}[]=,./,.'";
