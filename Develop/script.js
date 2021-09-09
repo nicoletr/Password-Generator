00644aeb00e267bbf23cf7afa67dfdfc555ae990
Nicoletr <nicole.trpevski@gmail.com>
2021-09-09 13:09:39 +0800

    Cleaned up unnecessary code and moved variables outside of the writePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength = window.prompt("How long would you like your password to be? Choose a number between 8 and 128")
+
+  // Determines users desired length of password
+  if (passwordLength < 8 && passwordLength > 128) {
+   passwordLength = prompt("Number must be between 8 and 128")
+  }
+
+  if (passwordLength > 8 && passwordLength < 128) {
+  //Determines if user would like to include a number in the password
+  var includeNumber = window.confirm("Would you like to include a number?")
+  //Determines if user would like to incluse a special character in the password
+  var includeSpecialCharacter = confirm("Would you like to include a special character?")
+  // Determines if user would like upper case letters in the password
+  var includeUpperCase = confirm("Would you like to include upper case letters?")
+  // Determines if user would like lower case letters in the password
+  var includeLowerCase = confirm("Would you like to include lower case letters?")
+  }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  // Determines users desired length of password
-  var passwordLength = window.prompt("How long would you like your password to be? Choose a number between 8 and 128")
-  var length = +passwordLength.value
-  if (passwordLength < 8 && passwordLength > 128) {
-   passwordLength = prompt("Please choose a number between 8 and 128")
-  }else {
-    console.log (length);
-  }
-
-  //Determines if user would like to include a number in the password
-  var includeNumber = window.confirm("Would you like to include a number?")
-   if (includeNumber) {
-    console.log (includeNumber);
-  }
-
-  //Determines if user would like to incluse a special character in the password
-  var includeSpecialCharacter = confirm("Would you like to include a special character?")
-  if (includeSpecialCharacter) {
-   console.log (includeSpecialCharacter);
-  }
-
-  // Determines if user would like upper case letters in the password
-  var includeUpperCase = confirm("Would you like to include upper case letters?")
-  if (includeUpperCase) {
-    console.log (includeUpperCase);
-  }
-
-  // Determines if user would like lower case letters in the password
-  var includeLowerCase = confirm("Would you like to include lower case letters?")
-  if (includeLowerCase) {
-    console.log (includeLowerCase);
-  }
 
   passwordText.value = password;
 
